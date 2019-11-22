9aed17429e3d509167d297461987cb76984de3f6
Carlos Miranda <carlos.miranda@unavarra.es>
2019-11-22 13:55:45 +0100

    V0.2.2 Bugfixes

--- R/hourly_RH.R
+++ R/hourly_RH.R
@@ -49,7 +49,7 @@ hourly_RH <- function(climdata,lat)
     mutate(Date = make_date(Year, Month, Day)) %>%
     select("Date","RHmax","RHmin")
   climdata_h = merge(temp.df, RH.df, by = "Date", all = TRUE) %>%
-    mutate(RH_aux = RHmax +(Temp-Tmin)*(RHmin-RHmax)/(Tmax-Tmin),
+    mutate(RH_aux = RHmax +((Temp-Tmin)/(Tmax-Tmin))*(RHmin-RHmax),
            RH = ifelse(RH_aux>RHmax,RHmax,
                        ifelse(RH_aux<RHmin,RHmin,RH_aux))) %>%
     select(Date,Year,Month,Day,DOY,Hour,Temp,RH)
